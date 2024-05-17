561e3fd95fb11405eae42e2ac636c6290d7862c4
paulovitorguedes <vitor_cguedes@outlook.com>
2024-05-17 20:28:46 -0300

    Add vendor compose

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'paulovitor/miniframework',
+        'pretty_version' => '2.7.6',
+        'version' => '2.7.6.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'paulovitor/miniframework' => array(
+            'pretty_version' => '2.7.6',
+            'version' => '2.7.6.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
